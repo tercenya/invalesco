493694c6c92155b8ea94c0932bfb1b30d36e1d0c
Craig M. Wellington <cmwellington@tamu.edu>
2015-04-13 16:59:36 -0500

    use masteriest to estimate player level
    also add tier and champion and winner accessors

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,6 +2,29 @@ class Participant
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
+  embeds_many :masteries
+
   field :teamId, type: Integer
   field :championId, type: Integer
+
+  def champion
+    Champion.find(championId)
+  end
+
+  def winner?
+    stats['winner']
+  end
+
+  def tier
+    highestAchievedSeasonTier.downcase.to_sym
+  end
+
+  # use mastery page to estimate the player's level
+  def estimated_level
+    # @NOTE: odd assuption:
+    #  if your mastery page was entirely blank, we're going to guess level 30.
+    return 30 if masteries.empty?
+
+    masteries.map(&:rank).reduce(:+)
+  end
 end
