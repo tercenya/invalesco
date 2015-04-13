1b2a6183fff7087784416e87c0b5b28a108fbfab
Craig M. Wellington <cmwellington@tamu.edu>
2015-04-13 16:57:40 -0500

    cache searchs on champion

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -2,8 +2,17 @@ class Champion
   include Mongoid::Document
   # include Mongoid::Attributes::Dynamic
 
+  @@all = []
+  @@cache = {}
+
   field :key, type: String
   field :name, type: String
   field :title, type: String
   field :_id, type: Integer, default: ->{ id }
-end
+
+
+  def self.find(id)
+    return @@cache[id] ||= super(id) if id.is_a?(Integer)
+    super
+  end
+end	
