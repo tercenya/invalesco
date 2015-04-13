eca8bf28b658cb401a702e472d05eb9f21411ee3
Craig M. Wellington <cmwellington@tamu.edu>
2015-04-13 16:59:54 -0500

    optimize team lookups for a match

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,4 +8,43 @@ class Match
 
   embeds_many :teams
   embeds_many :participants
+
+  def blue_team
+    teams[0]
+  end
+
+  def red_team
+    teams[1]
+  end
+
+  def blue_participants
+    #participants.where(teamId: 100)
+    # optimization: blue team is always the first 5
+    participants[0..4]
+  end
+
+  def red_participants
+    # participants.where(teamId: 200)
+    participants[5..9]
+  end
+
+  def champions
+    participants.map(&:champion)
+  end
+
+  def blue_characters
+    blue_participants.map(&:champion)
+  end
+
+  def red_characters
+    red_participants.map(&:champion)
+  end
+
+  def winner
+    blue_team.winner ? :blue : :red
+  end
+
+  def combined_levels
+    participants.map(&:estimated_level).reduce(:+)
+  end
 end
